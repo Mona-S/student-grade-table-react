b9c6b19ff45e8effaffb367889d7a19af8f049a3
Mona-S <forward2cm@gmail.com>
2019-08-18 01:29:39 +0000

    Completed view-average-grade feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,21 +5,35 @@ import GradeTable from './gradetable';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { grades: [] };
+    this.state = { grades: [],
+      avg: 0 };
   }
 
   componentDidMount() {
     fetch('/api/grades')
       .then(response => response.json())
-      .then(data => this.setState({ grades: data }));
+      .then(data => {
+        this.setState({ grades: data });
+      });
+  }
+
+  getAverageGrade() {
+    let gradeSum = 0;
+    let allGrades = this.state.grades;
+
+    for (let i = 0; i < allGrades.length; i++) {
+      gradeSum += allGrades[i].grade;
+    }
+    let average = (gradeSum / allGrades.length).toFixed(2);
+    return average;
   }
 
   render() {
     return (
-      <div>
-        <Header></Header>
+      <React.Fragment>
+        <Header calculatedAverage={this.getAverageGrade()}></Header>
         <GradeTable grades={this.state.grades}></GradeTable>
-      </div>
+      </React.Fragment>
     );
   }
 }
